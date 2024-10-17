a6ec511b520bfca1d54e3ff371a6ffa44a4f6e00
fukayatti0 <st24162yk@ibaraki.kosen-ac.jp>
2024-10-17 20:24:36 +0900

    javascriptの動的インポートを追加し、拡張子を統一しました

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,12 @@
 'use client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/globals.css';
+
+import { useEffect } from 'react';
+import { Raleway, Monomaniac_One, Noto_Serif_JP } from 'next/font/google';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import { Raleway, Monomaniac_One, Noto_Serif_JP } from 'next/font/google';
-import { useEffect } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/globals.css';
 
 const raleway = Raleway({ subsets: ['latin'], weight: '700' });
 const monomaniacOne = Monomaniac_One({ subsets: ['latin'], weight: '400' });
@@ -12,16 +14,20 @@ const notoSerifJP = Noto_Serif_JP({ subsets: ['latin'], weight: '700' });
 
 export default function RootLayout({ children }) {
   useEffect(() => {
-    // Bootstrap JSは既にインポートされているため、ここでのrequireは不要です。
+    import('bootstrap/dist/js/bootstrap.bundle.min.js')
+      .then(() => {
+        console.log('Bootstrap JS loaded');
+      })
+      .catch((err) => {
+        console.error('Failed to load Bootstrap JS', err);
+      });
   }, []);
 
   return (
     <html lang="ja">
       <body className={raleway.className}>
         <Navbar />
-        <main className="container my-5">
-          {children}
-        </main>
+        <main className="container my-5">{children}</main>
         <Footer />
       </body>
     </html>
